109fac66757855237c951614d71aa7c7bba7c122
Andy Jones <andy.jones@digital.education.gov.uk>
2023-02-04 09:24:14 +0000

    pages for request journey

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,36 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+var NotifyClient = require('notifications-node-client').NotifyClient,
+  notify = new NotifyClient(process.env.NOTIFYAPIKEY)
+
 // Add your routes here
+
+router.post('/sprint-3/request/process-request', function (req, res) {
+  // Send some emails through Notify
+
+  notify
+    .sendEmail(process.env.SATTemplateId, process.env.recipient, {
+      personalisation: {
+        title: req.session.data['title'],
+        summary: req.session.data['purpose'],
+      },
+    })
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err))
+
+  // This is the URL the users will be redirected to once the email
+  // has been sent
+  res.redirect('/sprint-3/request/submitted')
+})
+
+
+
+
+
+// Make sure this is after any routes for /manage
+router.get('/sprint-3/manage/:id', function(req, res){
+
+res.render('manage/entry/index')
+
+})
