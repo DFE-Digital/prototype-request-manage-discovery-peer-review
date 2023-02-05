c22f62acf4484af450678917f32a5702e1d9d698
Andy Jones <andy.jones@digital.education.gov.uk>
2023-02-05 22:18:44 +0000

    pages for request journey

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,8 @@ const router = govukPrototypeKit.requests.setupRouter()
 var NotifyClient = require('notifications-node-client').NotifyClient,
   notify = new NotifyClient(process.env.NOTIFYAPIKEY)
 
+const { v4: uuidv4 } = require('uuid')
+
 // Add your routes here
 
 router.post('/sprint-3/request/process-request', function (req, res) {
@@ -16,6 +18,16 @@ router.post('/sprint-3/request/process-request', function (req, res) {
 
   notify
     .sendEmail(process.env.SATTemplateId, process.env.recipient, {
+      personalisation: {
+        title: req.session.data['title'],
+        summary: req.session.data['purpose'],
+      },
+    })
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err))
+
+  notify
+    .sendEmail(process.env.SATTemplateId, process.env.recipient2, {
       personalisation: {
         title: req.session.data['title'],
         summary: req.session.data['purpose'],
@@ -34,18 +46,87 @@ router.get('/sprint-3/manage/', function (req, res) {
 })
 
 router.get('/sprint-3/manage/team/:id', function (req, res) {
-  res.render('sprint-3/manage/team/index.html')
+    var artefacts = []
+    if (req.session.data['artefacts'] !== undefined) {
+      artefacts = req.session.data['artefacts']
+    }
+  res.render('sprint-3/manage/team/index.html', { artefacts })
 })
 
 router.get('/sprint-3/manage/review/:id', function (req, res) {
-  res.render('sprint-3/manage/review/index.html')
+    var artefacts = []
+    if (req.session.data['artefacts'] !== undefined) {
+      artefacts = req.session.data['artefacts']
+    }
+  res.render('sprint-3/manage/review/index.html', { artefacts })
 })
 
 router.get('/sprint-3/manage/files/:id', function (req, res) {
-    res.render('sprint-3/manage/files/index.html')
+  var artefacts = []
+  if (req.session.data['artefacts'] !== undefined) {
+    artefacts = req.session.data['artefacts']
+  }
+
+  res.render('sprint-3/manage/files/index.html', { artefacts })
+})
+
+router.get('/sprint-3/manage/actions/:id', function (req, res) {
+    var artefacts = []
+    if (req.session.data['artefacts'] !== undefined) {
+      artefacts = req.session.data['artefacts']
+    }
+  res.render('sprint-3/manage/actions/index.html', { artefacts })
+})
+
+router.get('/sprint-3/manage/artefacts/add/:id', function (req, res) {
+    var artefacts = []
+    if (req.session.data['artefacts'] !== undefined) {
+      artefacts = req.session.data['artefacts']
+    }
+  res.render('sprint-3/manage/artefacts/add.html', { artefacts })
+})
+
+router.post('/sprint-3/manage/artefacts/add/:id', function (req, res) {
+  var id = req.params.id
+
+  let date_ob = new Date()
+  let date = ('0' + date_ob.getDate()).slice(-2)
+  let month = date_ob.toLocaleString('default', { month: 'short' });
+  let year = date_ob.getFullYear()
+  let hours = date_ob.getHours()
+  let minutes = date_ob.getMinutes()
+
+  // Create an empty array
+  var artefacts = []
+
+  // Check if there's an array in session
+  if (req.session.data['artefacts'] !== undefined) {
+    artefacts = req.session.data['artefacts']
+  }
+
+  artefacts.push({
+    appid: id,
+    url: req.session.data['artefact-url'],
+    title: req.session.data['artefact-title'],
+    addedOn: (date + " " + month + " " + year + " at " + hours + ":" + minutes),
+    addedBy: 'Andy Jones',
+    id: uuidv4(),
   })
+
+  req.session.data['artefacts'] = artefacts
+
+  // Create item into a session object and redirect to the artefacts page
+
+  console.log(artefacts)
+
+  res.redirect('/sprint-3/manage/files/' + id)
+})
 
 // Make sure this is after any routes for /manage
 router.get('/sprint-3/manage/:id', function (req, res) {
-  res.render('sprint-3/manage/entry/index.html')
+    var artefacts = []
+    if (req.session.data['artefacts'] !== undefined) {
+      artefacts = req.session.data['artefacts']
+    }
+  res.render('sprint-3/manage/entry/index.html', { artefacts })
 })
