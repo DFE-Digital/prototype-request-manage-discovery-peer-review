2defa8a9e7e8ac9b901998558c50c9a9ab13ea11
Andy Jones <andy.jones@digital.education.gov.uk>
2023-02-06 11:08:41 +0000

    pages for request journey

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,29 +3,7 @@
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
-/* eslint-disable no-new */
-// DfE frontend components
-import MenuToggle from 'dfeuk-frontend/packages/components/header/menuToggle';
-import SearchToggle from 'dfeuk-frontend/packages/components/header/searchToggle';
-import SkipLink from 'dfeuk-frontend/packages/components/skip-link/skip-link';
-import Details from 'dfeuk-frontend/packages/components/details/details';
-import Checkboxes from 'dfeuk-frontend/packages/components/checkboxes/checkboxes';
-import Radios from 'dfeuk-frontend/packages/components/radios/radios';
-import Card from 'dfeuk-frontend/packages/components/card/card';
 
-import AutoComplete from './autocomplete/autoCompleteConfig';
-
-// DfE frontend polyfills
-import 'dfeuk-frontend/packages/polyfills';
-
-// DfE digital service manual components
-import DesignExample from './design-example';
-import {
-  inputValue,
-  onConfirm,
-  source,
-  suggestion,
-} from './search';
 
 
 window.GOVUKPrototypeKit.documentReady(() => {
