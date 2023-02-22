07fdbc4c7816c684e8d2b0309b69d82414057a27
Andy Jones <andy.jones@digital.education.gov.uk>
2023-02-22 16:40:09 +0000

    updates

--- app/filters.js
+++ app/filters.js
@@ -1,7 +1,11 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
+const dateFilter = require('nunjucks-date-filter');
 
 addFilter('split', function(str, seperator) {
     return str.split(seperator);
 });
+
+
+addFilter('date', dateFilter);
